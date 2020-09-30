e6bde8f877da866b71492dd35073b96a362935a3
Rashi M <49615234+rashiraffi@users.noreply.github.com>
2020-10-01 00:09:02 +0530

    Added Regular expression cmd

--- regu.py
+++ regu.py
@@ -1,6 +1,7 @@
 import re
 
 str = "The rain in Spain"
+#Regular expression search
 x = re.search("(\w+\s*\w+)\s", str)
 
 print("The second white-space character is located in position:", x.start()) 
